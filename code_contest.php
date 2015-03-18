5a126121217e3ebf35c678ed6fd2872d1014d307
Vincent Peters <I>
2015-03-18 09:26:45 +0100

    Copy changes

--- code_contest.php
+++ code_contest.php
@@ -39,6 +39,8 @@ License:
  * TEMP solution for php errors
  */
 if ( gethostname() == "Vincents-MBP.telenet.be" ) {
+
+	require_once('vendor/autoload.php');
 	ini_set( 'display_errors', 1 );
 	ini_set( 'display_startup_errors', 1 );
 	error_reporting( - 1 );
